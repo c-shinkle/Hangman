cb8253cd1937b2968a74236ac08935e055d73967
Christian <cshinkle2018@gmail.com>
2019-12-21 14:02:10 -0600

    Implemented a game turn

--- src/Hangman.py
+++ src/Hangman.py
@@ -4,3 +4,12 @@ def check(word, guess):
       return True
   return False
 
+def get_input():
+  return input("What's your guess? ")
+
+def game_turn(word):
+  guess = get_input()
+  if check(word, guess):
+    return True
+  else:
+    return False
--- test/test_Hangman.py
+++ test/test_Hangman.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest.mock import patch
 from src.Hangman import check
+from src.Hangman import game_turn
 
 class HangmanTests(unittest.TestCase):
   def test_check_if_letter_is_in_word(self):
@@ -10,5 +12,10 @@ class HangmanTests(unittest.TestCase):
     for letter in "Christian":
       self.assertTrue(check("Christian", letter))
 
+  @patch("src.Hangman.get_input", return_value="C")
+  def test_when_user_guesses_letter_game_checks_letter(self, input):
+    self.assertTrue(game_turn("Christian"))
+  
+  
 if __name__ == '__main__':
     unittest.main()
