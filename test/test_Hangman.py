82bcd66597ac0150f64944e94e4a6685b0ef2a74
Christian <cshinkle2018@gmail.com>
2019-12-21 13:45:32 -0600

    Added test for checking whole word

--- test/test_Hangman.py
+++ test/test_Hangman.py
@@ -1,10 +1,14 @@
 import unittest
-from src import Hangman
+from src.Hangman import check
 
 class HangmanTests(unittest.TestCase):
   def test_check_if_letter_is_in_word(self):
-    containsLetter = Hangman.check("Christian", "C")
+    containsLetter = check("Christian", "C")
     self.assertEqual(containsLetter, True)
+
+  def test_when_all_letter_are_guessed_they_all_return_true(self):
+    for letter in "Christian":
+      self.assertTrue(check("Christian", letter))
 
 if __name__ == '__main__':
     unittest.main()
