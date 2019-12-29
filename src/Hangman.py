529fd5f5d843b033b7448af62259c947f22748d1
Christian Shinkle <shinkle.christian@principal.com>
2019-12-28 20:33:03 -0600

    Added win functionality

--- src/Hangman.py
+++ src/Hangman.py
@@ -7,21 +7,30 @@ def check(word, guess):
 def get_input():
   return input("What's your guess? ")
 
-def game_turn(word):
+def game_turn(word, letters):
   guess = get_input()
   if check(word, guess):
-    print("Good job! {} is in the word".format(guess))
+    print("Good job! '{}' is in the word".format(guess))
+    letters[guess] = False
     return True
   else:
-    print("Tough luck, {} is not the word".format(guess))
+    print("Tough luck, '{}' is not the word".format(guess))
     return False
+
+def createLookupDict(word):
+  letters = dict.fromkeys('abcdefghijklmnopqrstuvwxyz', False)
+  for letter in word:
+    letters[letter] = True
+  return letters
 
 def game_loop(word):
   misses = 0
+  letters = createLookupDict(word)
   while (misses < 3):
-    correct = game_turn(word)
+    correct = game_turn(word, letters)
     if (correct):
-      return True
+      if (all(not value for value in letters.values())):
+        return True
     else:
       misses+=1
   return False
--- test/test_Hangman.py
+++ test/test_Hangman.py
@@ -4,8 +4,7 @@ from src.Hangman import check, game_turn, game_loop
 
 class HangmanTests(unittest.TestCase):
   def test_when_letter_is_guessed_it_returns_true(self):
-    containsLetter = check("Christian", "C")
-    self.assertTrue(containsLetter)
+    self.assertTrue(check("Christian", "C"))
 
   def test_when_all_letter_are_guessed_they_all_return_true(self):
     for letter in "Christian":
@@ -13,13 +12,17 @@ class HangmanTests(unittest.TestCase):
 
   @patch("src.Hangman.get_input", return_value="C")
   def test_when_user_guesses_letter_game_checks_letter(self, input):
-    self.assertTrue(game_turn("Christian"))
+    self.assertTrue(game_turn("Christian", {}))
   
   @patch("src.Hangman.get_input")
   def test_when_user_guesses_three_wrong_game_is_over(self, mock_get_input):
     mock_get_input.side_effect = ['a', 'b', 'c']
     self.assertFalse(game_loop("xyz"))
 
-  
+  @patch("src.Hangman.get_input")
+  def test_when_user_guesses_all_letters_game_is_over(self, mock_get_input):
+    mock_get_input.side_effect = ['x', 'y', 'z']
+    self.assertTrue(game_loop("xyz"))
+
 if __name__ == '__main__':
     unittest.main()
