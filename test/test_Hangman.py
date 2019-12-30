e827658ec3ab009cdcc4c2a941f49956e29925ce
Christian Shinkle <shinkle.christian@principal.com>
2019-12-29 21:56:21 -0600

    Refactored to remove redundant strings

--- test/test_Hangman.py
+++ test/test_Hangman.py
@@ -3,17 +3,18 @@ from unittest.mock import patch, Mock, call
 from src.Hangman import check, game_turn, game_loop, createLookupDict
 
 class HangmanTests(unittest.TestCase):
+  test_string = "Christian"
   def test_when_letter_is_guessed_it_returns_true(self):
-    self.assertTrue(check(createLookupDict("Christian"), "c"))
+    self.assertTrue(check(createLookupDict(self.test_string), "c"))
 
   def test_when_all_letter_are_guessed_they_all_return_true(self):
-    letters = createLookupDict("Christian")
-    for letter in "christian":
+    letters = createLookupDict(self.test_string)
+    for letter in self.test_string.lower():
       self.assertTrue(check(letters, letter))
 
   @patch("src.Hangman.get_input", return_value="c")
   def test_when_user_guesses_letter_game_checks_letter(self, mock_get_input):
-    self.assertTrue(game_turn(createLookupDict("Christian")))
+    self.assertTrue(game_turn(createLookupDict(self.test_string)))
   
   @patch("src.Hangman.get_input", side_effect=['a', 'b', 'c'])
   def test_when_user_guesses_three_wrong_game_is_over(self, mock_get_input):
