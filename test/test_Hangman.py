f2674d9f0ac4201f6b6b5563e715bad66154920a
Christian Shinkle <shinkle.christian@principal.com>
2019-12-30 21:09:55 -0500

    Added more assertions to tests

--- test/test_Hangman.py
+++ test/test_Hangman.py
@@ -10,23 +10,28 @@ class HangmanTests(unittest.TestCase):
   def test_when_all_letter_are_guessed_they_all_return_true(self):
     letters = createLookupDict(self.test_string)
     for letter in self.test_string.lower():
-      self.assertTrue(check(letters, letter))
+      with self.subTest(letter=letter):
+        self.assertTrue(check(letters, letter))
 
   @patch("src.Hangman.get_input", return_value="c")
   def test_when_user_guesses_letter_game_checks_letter(self, mock_get_input):
     self.assertTrue(game_turn(createLookupDict(self.test_string)))
+    self.assertTrue(mock_get_input.called)
   
   @patch("src.Hangman.get_input", side_effect=['a', 'b', 'c'])
   def test_when_user_guesses_three_wrong_game_is_over(self, mock_get_input):
     self.assertFalse(game_loop("xyz"))
+    self.assertEquals(3, mock_get_input.call_count)
 
   @patch("src.Hangman.get_input", side_effect=['x', 'y', 'z'])
   def test_when_user_guesses_all_letters_game_is_over(self, mock_get_input):
     self.assertTrue(game_loop("xyz"))
+    self.assertEquals(3, mock_get_input.call_count)
 
   @patch("src.Hangman.get_input", return_value="x")
   def test_when_user_guesses_uppercase_letters_game_allows_it(self, mock_get_input):
     self.assertTrue(game_turn(createLookupDict("XYZ")))
+    self.assertTrue(mock_get_input.called)
 
 if __name__ == '__main__':
     unittest.main()
