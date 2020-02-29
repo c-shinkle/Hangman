051c54f8afa2c0eca65ed712f0fde1b0f8068752
Christian <cshinkle2018@gmail.com>
2020-02-28 21:07:11 -0600

    Formatted code, added .idea to .gitignore

--- src/Hangman.py
+++ src/Hangman.py
@@ -1,42 +1,46 @@
 def get_input():
-  return input("What's your guess? ")
+    return input("What's your guess? ")
+
 
 def game_turn(letters_left_to_guess, guessed_letters):
-  guess = get_input().lower()
-  if guess in guessed_letters:
-    print("You already guessed '{}'! Try again".format(guess))
-    return guess
-  elif guess in letters_left_to_guess:
-    print("Good job! '{}' is in the word".format(guess))
-    letters_left_to_guess.remove(guess)
-    guessed_letters.add(guess)
-    return guess
-  else:
-    print("Tough luck, '{}' is not the word".format(guess))
-    return False
+    guess = get_input().lower()
+    if guess in guessed_letters:
+        print("You already guessed '{}'! Try again".format(guess))
+        return guess
+    elif guess in letters_left_to_guess:
+        print("Good job! '{}' is in the word".format(guess))
+        letters_left_to_guess.remove(guess)
+        guessed_letters.add(guess)
+        return guess
+    else:
+        print("Tough luck, '{}' is not the word".format(guess))
+        return False
+
 
 def create_lookup_set(word):
-  return set(char for char in word.lower())
+    return set(char for char in word.lower())
+
 
 def create_letter_indices_list(word, letter):
-  return list(pos for pos, char in enumerate(word) if char == letter)
+    return list(pos for pos, char in enumerate(word) if char == letter)
+
 
 def game_loop(word):
-  word = word.lower()
-  misses = 0
-  letters_left_to_guess = create_lookup_set(word)
-  guessed_letters = set()
-  so_far = list('_' * len(word))
-  while misses < 3:
-    print("".join(so_far))
-    result = game_turn(letters_left_to_guess, guessed_letters)
-    if result:
-      indices = create_letter_indices_list(word, result)
-      for index in indices:
-        so_far[index] = result
-      if not letters_left_to_guess:
-        print("Congratulations, you win! The word was {}".format(word))
-        return True
-    else:
-      misses+=1
-  return False
+    word = word.lower()
+    misses = 0
+    letters_left_to_guess = create_lookup_set(word)
+    guessed_letters = set()
+    so_far = list('_' * len(word))
+    while misses < 3:
+        print("".join(so_far))
+        result = game_turn(letters_left_to_guess, guessed_letters)
+        if result:
+            indices = create_letter_indices_list(word, result)
+            for index in indices:
+                so_far[index] = result
+            if not letters_left_to_guess:
+                print("Congratulations, you win! The word was {}".format(word))
+                return True
+        else:
+            misses += 1
+    return False
